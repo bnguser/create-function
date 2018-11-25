9bb05d4f9ae2827ee6c39566c0094f1324d3af0f
bnguser <batngoal@gmail.com>
2018-11-25 18:40:21 +0530

    working with props

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, i'm react app</h1>
         <p>This is really working</p>
-        <Person />
-        <Person />
-        <Person />
+        <Person name ='max' age ='28'/>
+        <Person name = 'manu' age ='29'>My hobbies : Racing</Person>
+        <Person name = 'sathish' age = '30'/>
         <Person />
       </div>
     );
